7aa03f51e4a97cdfeabcaed47279487a7ecb0f4f
Cuaju <cuahu3000@gmail.com>
2025-03-06 14:48:25 -0600

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MelodiasDelMundo_Client.ServiceReference1;
+
+namespace MelodiasDelMundo_Client
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UsersManagerClient _service;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _service=new UsersManagerClient();
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+
+            if (_service.LogIn(username, password))
+            {
+                Console.WriteLine("Logged in nigga");
+            }
+            else 
+            {
+                Console.WriteLine("nigga you dumb");
+            }
+        }
+    }
+}
